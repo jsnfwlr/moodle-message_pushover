467ffc09b6ae1c7e2c15ed946c46c86d1728f925
Jason Fowler <phalacee@gmail.com>
2013-09-13 12:40:54 +0800

    Adding maturity details

--- version.php
+++ version.php
@@ -15,7 +15,8 @@ defined('MOODLE_INTERNAL') || die('File cannot be accessed directly.');
 
 $plugin->component      = 'message_pushover';
 $plugin->version        = 2013091004;
+$plugin->maturity	= 'MATURITY_BETA';
 $plugin->requires       = 2013040500;
 $plugin->release	= '1.0 (Build: 2013091004)';
-$plugin->maturity	= 'MATURITY_BETA';
+
 
